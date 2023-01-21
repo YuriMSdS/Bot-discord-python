47bd816da9fd3e248f775c166f478ccf608063fa
YuriMSdS <yuri.md13@gmail.com>
2023-01-21 10:59:16 -0300

    correção de comandos e alguns novos

--- autobot.py
+++ autobot.py
@@ -14,22 +14,37 @@ async def on_message(message):
     if message.author == client.user:
         return
  
-    if message.content == '!hi':
-        await message.channel.send('Hello!')
+    if message.content == 'Oi bot':
+        await message.channel.send('Falatu!')
+
+    if message.content == 'tudo bem bot ?':
+       await message.channel.send('to top e tu?')
+
+    if message.content == 'to bem tbm':
+        await message.channel.send('que bom!')  
  
+    if message.content == '!apresentação':
+        await message.channel.send('olá eu sou o AutoBot, fui criado pelo yuri como primeiro projeto de programação, sou amigável!!! É um prazer te conhecer!!!!')
+    
+    if message.content == 'AutoBot se apresente':
+       await message.channel.send('olá eu sou o AutoBot, fui criado pelo yuri como primeiro projeto de programação, sou amigável!!! É um prazer te conhecer!!!!')   
+
     if message.content == '!image':
         await message.channel.send(file=discord.File('download.jpg'))
 
-    if message.content == '!contratar':
+    if message.content == 'bot quem devo contratar ?':
         await message.channel.send('https://www.linkedin.com/in/yuri-silva-96a115218/') 
 
-    if message.content == '!aprender_nuvem':
+    if message.content == '!nuvem':
        await message.channel.send('https://github.com/YuriMSdS')
 
     if message.content == '!developer':
         with open('foto dev (eu).jpeg','rb')as f:  
          await message.channel.send(file=discord.File('foto dev (eu).jpeg'))
 
+    if message.content == 'quem é o espuminha?':
+        await message.channel.send('Fuja')
+    
     if message.content == '!curiosidade':
         await message.author.send('Foi uma mulher que inventou a programação!! O primeiro algoritmo de programação foi criado por Ada Lovelace, em 1833, no processo de construção de uma máquina analítica.')
 
